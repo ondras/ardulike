99de70e0564d491a3c94cf9370073bd8902fc248
Balazs Kutil <balazs@kutilovi.cz>
2014-04-22 14:30:58 +0200

    Add deaths count property to player

--- ardulike/scripts/entities.rb
+++ ardulike/scripts/entities.rb
@@ -1,6 +1,6 @@
-Player = Struct.new(:toughness, :hp, :hp_max, :exp, :exp_next_level, :level, :hp_mean, :kills, :died_in_turn) do
+Player = Struct.new(:toughness, :hp, :hp_max, :exp, :exp_next_level, :level, :hp_mean, :kills, :deaths, :died_in_turn) do
   def to_s
-    "[#{self.class.name.downcase}] lvl: #{level} exp: #{exp}/#{exp_next_level} toughness: #{toughness} hp: #{hp}/#{hp_mean}/#{hp_max} kills: #{kills} #{"died: #{died_in_turn}" if died_in_turn}"
+    "[#{self.class.name.downcase}] lvl: #{level} exp: #{exp}/#{exp_next_level} toughness: #{toughness} hp: #{hp}/#{(hp_mean * 100).to_i / 100.0}/#{hp_max} kills: #{kills} deaths: #{deaths} #{"died: #{died_in_turn}" if died_in_turn}"
   end
 end
 
