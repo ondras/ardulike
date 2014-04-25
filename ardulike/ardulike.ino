d41c5e56688699ba0a3b73321f554a74a594ed69
Balazs Kutil <balazs@kutilovi.cz>
2014-04-26 00:02:50 +0200

    Drop unused entities

--- ardulike/ardulike.ino
+++ ardulike/ardulike.ino
@@ -9,14 +9,12 @@
 #include "stairs.h"
 #include "msgqueue.h"
 
-MsgQueue q = MsgQueue(9);
+MsgQueue q = MsgQueue(8);
 World w = World();
 
 Player   player    = Player(0, 0, 10, 3, 3);
 
-Npc ogre     = Npc(0, 2, 5, 2, 2, CHAR_OGRE);
-Npc orc      = Npc(1, 10, 3, 1, 2, CHAR_ORC);
-Npc dog      = Npc(1, 2 , 1, 1, 2, CHAR_DOG);
+Npc ogre     = Npc(0, 2, 5, 1, 3, CHAR_OGRE);
 
 Stairs stairs_down = Stairs(0, 25, 1, 6);
 Stairs stairs_up   = Stairs(1, 6,  0, 25);
@@ -27,9 +25,11 @@ Entity wall        = Entity(0, 21, CHAR_WALL, ENTITY_TERRAIN | ENTITY_BLOCKS_MOV
 Screen screen = Screen();
 
 void setup(void) {
+  randomSeed(analogRead(A2));
+
   w.setOutput(&q);
   w.addPlayer(&player);
-  w.addEntities(8, &player, &ogre, &stairs_up, &stairs_down, &orc, &dog, &wall, &tree);
+  w.addEntities(6, &player, &ogre, &stairs_up, &stairs_down, &wall, &tree);
   w.init();
   screen.render(&w, &q);
 }
